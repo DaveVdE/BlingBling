febce9b71422789fabbfee1093ec1f0a170677c8
Dave Van den Eynde <dave@groovbird.com>
2014-03-01 07:27:21 +0100

    Added Flash

---
+++ Flash.h
@@ -0,0 +1,19 @@
+
+struct Sparkle
+{
+  uint8_t speed;
+  
+  Sparkle(uint8_t speed) : speed(speed) 
+  {
+  }
+  
+  Pixel operator()(int frame, int index)
+  {
+    if (frame % speed = 0) {
+      return Pixel(255, 255, 255);
+    } else {
+      return Pixel(0, 0, 0);
+    }
+  }  
+
+};
